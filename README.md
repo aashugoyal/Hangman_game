3918ed70febae84ec33e54b49d32223581cc7b91
Ashu Goyal <ashu125001@gmail.com>
2018-01-20 02:36:40 +0530

    changed

---
+++ README.md
@@ -0,0 +1,7 @@
+#HANGMAN GAME
+
+
+Its a game where user guess the word. if it correct you won.
+
+
+I am currently working on this.
---
+++ hangman.py
@@ -0,0 +1,36 @@
+import random
+import string
+word= 'blue green yellow black brown red'.split()
+def chooseWord(word):
+    return random.choice(word)
+def isWordGuessed(secretWord, letterGuessed):
+    """
+       letterGuessed : the letter which we are guessing
+       if all the letter which we are guessing is in secretWord
+       then our function  return True otherwise False   
+    """
+    for x in range(len(secretWord)):
+        counter=0
+        if secretWord[x] in letterGuessed:
+            counter+=1
+        if counter == len(secretWord):
+            return True
+        else:
+            return False
+def getGuessedWord(secretWord, letterGuessed):
+    '''
+    if letter is correct then it is in the correct position
+    '''
+    value=''
+    char=''
+    for x in secretWord:
+        if x in letterGuessed:
+            value = x
+        else:
+            value='_'
+        char=char+value
+        return char
+    '''
+    currently working on it 
+    12345
+    '''
